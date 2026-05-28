0ac45a99af9506c18a72c35142583bf8ec0fec08
obidjonovbehzod9901 <obidjonovbehzod9901@gmail.com>
2026-05-28 19:56:20 +0500

    This is my first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const formCreate = document.getElementById('form-create')
+const formEdit = document.getElementById('form-edit')
+const listGroupTodo = document.getElementById('list-group-todo')
+// const messageCreate = document.getElementById('message-create')
+const time = document.getElementById('time')
+const modal = document.getElementById('modal')
+const overlay = document.getElementById('overlay')
+/* time elements */
+const fullDay = document.getElementById('full-day')
+const hourEl = document.getElementById('hour')
+const minuteEl = document.getElementById('minute')
+const secondEl = document.getElementById('second')
+const closeEl = document.getElementById('close')
